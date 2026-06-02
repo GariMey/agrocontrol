011f50d51e7df06e82be648b9f5ded9294e350fa
GariMey <melanygaritauu1276@gmail.com>
2026-06-01 21:25:24 -0600

    final

---
+++ agrocontrol/__init__.py
@@ -0,0 +1,1 @@
+# Archivo vacío está bien
--- cultivos/apps.py
+++ cultivos/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
 class CultivosConfig(AppConfig):
-    name = 'cultivos'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cultivos'  
